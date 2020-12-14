45b88f0c3cb432471e4ab86d8a06a18622f89720
aznoH333 <62841387+aznoH333@users.noreply.github.com>
2020-12-14 15:11:41 +0100

    added an onion to edit blogs

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -12,15 +12,36 @@ export class BlogComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  blogs = [
+  blogs = []
 
-  ]
+  blogToEdit;
+
   @ViewChild('titleInput') titleInput : ElementRef;
   @ViewChild('textInput') textInput : ElementRef;
+  @ViewChild('editTitleInput') editTitleInput : ElementRef;
+  @ViewChild('editTextInput') editTextInput : ElementRef;
 
   addBlog(){
     this.blogs.push([this.titleInput.nativeElement.value,this.textInput.nativeElement.value]);
     this.titleInput.nativeElement.value = "";
     this.textInput.nativeElement.value = "";
+  }
+
+  editBlog(){
+    console.log(this.editTitleInput.nativeElement.value);
+    if (this.editTitleInput.nativeElement.value !=""){
+      this.blogToEdit[0] = this.editTitleInput.nativeElement.value;
+    }
+    if (this.editTextInput.nativeElement.value !="") {
+      this.blogToEdit[1] = this.editTextInput.nativeElement.value;
+    }
+
+    this.editTitleInput.nativeElement.value = "";
+    this.editTextInput.nativeElement.value = "";
+    this.blogToEdit = null;
+  }
+
+  setBlogToEdit(blog){
+    this.blogToEdit = blog;
   }
 }
